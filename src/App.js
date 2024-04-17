c497110c8d15692f728b5c87e43357bb38c7187b
mzecevic04 <mzecevic04@gmail.com>
2024-04-17 02:20:39 +0200

    feat: test app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
